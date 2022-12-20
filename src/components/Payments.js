8e04e250539f2e6dcc4cfe2312d1973ff6a3624c
Matias Schmidt <matias.schmidt@outlook.es>
2022-12-20 00:15:03 -0300

    estilos payment

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -4,34 +4,42 @@ import "./CSS/payments.css";
 
 function Payments() {
   return (
-    <div className="item">
+    <div className="payment-container">
 
         <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/8826/8826099.png" width='40' alt="credito"/>
-            <p> Tarjeta de credito </p>
-            <p> Ver promociones bancarias </p>
+            <img src="https://cdn-icons-png.flaticon.com/512/8826/8826099.png" alt="credito" className="pay-img"/>
+            <div className="payment-text">
+                <p> Tarjeta de credito </p>
+                <p className="pay-subtext"> Ver promociones bancarias </p>
+            </div>
         </div>
 
         <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/8685/8685039.png" width='40' alt="debito"/>
-            <p> Tarjeta de debito </p>
-            <p> Ver mas </p>
+            <img src="https://cdn-icons-png.flaticon.com/512/8685/8685039.png" alt="debito" className="pay-img"/>
+            <div className="payment-text">
+                <p> Tarjeta de debito </p>
+                <p className="pay-subtext"> Ver mas </p>
+            </div>
         </div>
 
         <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/806/806843.png" width='40' alt="cuotas"/>
-            <p> Cuotas sin tarjeta </p>
-            <p> Conocé Mercado Credito </p>
+            <img src="https://cdn-icons-png.flaticon.com/512/806/806843.png" alt="cuotas" className="pay-img"/>
+            <div className="payment-text">
+                <p> Cuotas sin tarjeta </p>
+                <p className="pay-subtext"> Conocé Mercado Credito </p>
+            </div>
         </div>
 
         <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/4947/4947104.png" width='40' alt="efectivo"/>
-            <p> Efectivo </p>
-            <p> Ver mas </p>
+            <img src="https://cdn-icons-png.flaticon.com/512/4947/4947104.png" alt="efectivo" className="pay-img"/>
+            <div className="payment-text">
+                <p> Efectivo </p>
+                <p className="pay-subtext"> Ver mas </p>
+            </div>            
         </div>
 
-        <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/8212/8212684.png" width='40' alt="mas"/>
+        <div className="plus">
+            <img src="https://cdn-icons-png.flaticon.com/512/8212/8212684.png" className="pay-plus" alt="mas" />
         </div>
 
   </div>
