60d3533bd756b09c31fc50c05593a63b8417c9e3
Matias Schmidt <matias.schmidt@outlook.es>
2022-12-21 17:09:23 -0300

    css terminado componente subscription

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,7 +1,4 @@
 import React from "react";
-import disney from "../assets/Disney+_logo.svg.png";
-import star from "../assets/Star+_logo.svg.png";
-import truck from "../assets/truckgiftv4@2x.png";
 import "./CSS/subscription.css";
 
 
@@ -13,25 +10,31 @@ function Subscription() {
         <div className="container-subscription">
             <div className="subscription-top">
                 <h2>Suscribite al nivel 6</h2>
-                <p>  | $499/mes </p>
+                <div className="subscription-promo">
+                    <p className="subscription-num"><del>$ 1.439</del></p>
+                    <div className="subscription-algo">
+                        <p className="subscription-number">$499</p>
+                        <p className="subscription-num">/mes </p>
+                    </div>
+                </div>
             </div>
 
             <div className="susbscription-bottom">
-                <p> Conseguí los mejores beneficios en Mercado Libre </p>
+                <p className="subscription-text"> Conseguí los mejores beneficios en Mercado Libre </p>
 
                 <div className="subscription-options">
                     <div className="icon-subscription">
-                        <img src={disney} width='40' alt="Disney+"/>
+                        <img src="https://http2.mlstatic.com/resources/frontend/statics/loyal/partners/widget-l6/dplus.svg" className="circle-icon" alt="Disney+"/>
                         <p> Disney+ sin cargo </p>
                     </div>
 
                     <div className="icon-subscription">
-                        <img src={star} width='40' alt="Star+"/>
+                        <img src="https://http2.mlstatic.com/resources/frontend/statics/loyal/partners/widget-l6/starplus.svg" className="circle-icon" alt="Star+"/>
                         <p> Star+ sin cargo </p>
                     </div>
 
                     <div className="icon-subscription">
-                        <img src={truck} width='40' alt="Star+"/>
+                        <img src="https://http2.mlstatic.com/resources/frontend/statics/loyal/v2/truckgiftv4@2x.png" className="circle-icon" alt="Star+"/>
                         <p>  Envíos gratis y rápidos desde $ 5.500 y 45% OFF en envíos de menos de $ 5.500 </p>
                     </div>
                 </div>
