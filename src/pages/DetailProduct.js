6fcf0df7534c172b400203bb5a26c155768275ce
Matias Schmidt <matias.schmidt@outlook.es>
2023-01-12 15:11:13 -0300

    Empezando componente Detail product

--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -2,9 +2,17 @@ import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
 import { verDetail } from '../redux/actions.js';
-import {cleanSearch} from "../redux/actions.js"
+import {cleanSearch} from "../redux/actions.js";
+import ofertas2 from "../assets/ofertas2.png";
+import personal from "../assets/personal.png";
+import bebes from "../assets/bebes.png";
+import limpieza from "../assets/limpieza.png";
+import bebidas from "../assets/bebidas.png";
+import almacen from "../assets/almacen.png";
+import mascotas from "../assets/mascotas.png";
+import '../components/CSS/detail.css';
 
-//import './detail.css';
+
 
 function DetailProduct() {
   const dispatch = useDispatch()
@@ -22,7 +30,84 @@ function DetailProduct() {
 
   return (
     <div className='container-detail'>
-        <h1>{detail.title}</h1>
+
+        <div className='supermarket-detail'>
+
+          <h2 className='supermarket-title'> Supermercado </h2>
+
+            <div className='img-supermarket'>
+              <img src={ofertas2} alt="Ofertas" width="120" />
+              <p className='text-image'> Ofertas </p>
+            </div>
+
+            <div className='img-supermarket'>
+              <img src={personal} alt="Cuidado Personal" width="120" />
+              <p className='text-image'> Cuidado Personal</p>
+            </div>
+            
+            <div className='img-supermarket'>
+              <img src={bebes} alt="Bebes" width="120" />
+              <p className='text-image'> Bebes </p>
+            </div>
+
+            <div className='img-supermarket'>
+              <img src={limpieza} alt="Limpieza" width="120" />
+              <p className='text-image'> Limpieza </p>
+            </div>
+
+            <div className='img-supermarket'>
+              <img src={bebidas} alt="Bebidas" width="120" />
+              <p className='text-image'> Bebidas </p>
+            </div>
+
+            <div className='img-supermarket'>
+              <img src={almacen} alt="Almacen"  width="120" />
+              <p className='text-image'> Almacen </p>
+            </div>
+
+            <div className='img-supermarket'>
+              <img src={mascotas} alt="Mascotas" width="100" />
+              <p className='text-image'> Mascotas </p>
+            </div>
+        </div>
+
+        
+
+        <div className="img-detail">
+          <img src={detail.thumbnail} alt="imagen producto" width="150"/>
+          <h2>{detail.title}</h2>
+
+          <div className='detail-description'>
+            <h3> Caracteristicas principales </h3>
+            
+            <table>
+              <thead>
+                <tr className='table-header'>
+                  <th className='columna-header'> </th>
+                  <th className='columna-header-'> </th>
+                  <th className='columna-header'> </th>
+                  <th className='columna-header'> </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr >
+                  <td className='columna-respuesta'> {} </td>
+                  <td className='columna-respuesta'> {} </td>
+                  <td className='columna-respuesta'> {} </td>
+                  <td className='columna-respuesta'> {} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div>
+            Envío gratis a todo el país
+          </div>
+
+
+
+        </div>
     </div>
     
   );
